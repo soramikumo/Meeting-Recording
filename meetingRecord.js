028e882943da2eff4c9ccaf245abba3d7e2cfb6d
soramikumo <115201451+soramikumo@users.noreply.github.com>
2023-06-01 02:42:28 +0900

    バグの修正

--- meetingRecord.js
+++ meetingRecord.js
@@ -3,13 +3,15 @@ recognition.lang = "ja";
 recognition.continuous = true;
 recognition.interimResults = true;
 
-let output = document.querySelector('.output');
+const output = document.querySelector('.output');
+const output2 = document.querySelector('.output2');
+let recordedTranscript = ''; // 記録した結果を保持する変数
 
 recognition.onresult = function(event) {
   let finalTranscript = '';
   let interimTranscript = '';
-  
-  //前回の処理で処理済みの音声認識結果はスキップして、新しい音声認識結果を処理する
+
+  // 前回の処理で処理済みの音声認識結果はスキップして、新しい音声認識結果を処理する
   for (let i = event.resultIndex; i < event.results.length; i++) {
     const result = event.results[i];
     if (result.isFinal) {
@@ -20,12 +22,25 @@ recognition.onresult = function(event) {
   }
   
   output.textContent = finalTranscript + interimTranscript;
+  output2.textContent = recordedTranscript + finalTranscript;
+  
+  // 記録した結果を更新する
+  recordedTranscript = output2.textContent;
 };
+
 const startButton = document.querySelector(".start");
 startButton.addEventListener("click", () => {
   recognition.start();
 });
+
 const stopButton = document.querySelector(".stop");
 stopButton.addEventListener("click", () => {
   recognition.stop();
+});
+
+const clearButton = document.querySelector(".clear");
+clearButton.addEventListener("click", () => {
+  output.textContent = '';
+  output2.textContent = '';
+  recordedTranscript = '';
 });
